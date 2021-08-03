76f514fcb964fd8354a971a7b0bed6f4ac72bc34
Makktu <johnmcna@gmail.com>
2021-08-03 17:06:01 +0100

    First Commit – the setup phase

---
+++ README.md
@@ -0,0 +1,5 @@
+# LIBRARY APP
+
+Back with ODIN, and another project: The Great Library.
+
+Ostensibly a project to learn more about Objects and Object Prototypes in JavaScript, I will be paying close attention to the HTML/CSS side of things as well, and trying to come up with a decent design for the webpage.
---
+++ script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// * items in book library:
+// * TITLE
+// * AUTHOR
+// * FORMAT (paperback, digital etc.)
+// * PAGES
+// * READ
+
+let theLibrary = [];
+let bookTitle;
+let bookAuthor;
+let bookFormat;
+let bookPages;
+let bookIsRead;
+
+function Book() {
+    // the constructor
+}
+
+function addBookToLibrary() {
+    // stuff
+}
